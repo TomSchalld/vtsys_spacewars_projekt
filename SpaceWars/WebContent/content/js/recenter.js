467c38ee3c6c211a0688e482a7f81379d317657c
iPod90 <register@thomas-schalldach.de>
2015-07-11 23:50:14 +0200

    mthfruckin changes

---
+++ SpaceWars/WebContent/content/js/recenter.js
@@ -0,0 +1,14 @@
+function setMargins() {
+    width = $(window).width();
+    containerWidth = $("#shop").width();  
+    leftMargin = (width-containerWidth)/2;    
+    $("#shop").css("marginLeft", leftMargin);    
+    $("#report").css("marginLeft", leftMargin);    
+}
+
+$(document).ready(function() {
+    setMargins();
+    $(window).resize(function() {
+        setMargins();    
+    });
+});
--- SpaceWars/src/web/Gaming.java
+++ SpaceWars/src/web/Gaming.java
@@ -1,6 +1,8 @@
 package web;
 
 import java.io.IOException;
+import java.util.Map;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -12,7 +14,7 @@ import logic.Report;
 /**
  * Servlet implementation class Gaming
  */
-@WebServlet(asyncSupported = true, urlPatterns = { "/Gaming" })
+@WebServlet(asyncSupported = true)
 public class Gaming extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
@@ -28,15 +30,18 @@ public class Gaming extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		Map<String, String[]> params = request.getParameterMap();
+		for(String s[]:params.values()){
+			for(String string:s){
+				System.out.println(string);
+			}
+		}
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
 		doGet(request, response);
 	}
 	
