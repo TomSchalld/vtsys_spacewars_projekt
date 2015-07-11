41a059c4ac0d730a1a0483e69590da280a6989de
iPod90 <register@thomas-schalldach.de>
2015-07-12 01:29:46 +0200

    didid

--- SpaceWars/WebContent/content/js/gameFunctions.js
+++ SpaceWars/WebContent/content/js/gameFunctions.js
@@ -36,41 +36,70 @@ var fight = "Fighter";
 var priceFighter = 200;
 var priceBattlestar = 2000;
 var cash = 5000;
-
-function fighterPlus(planet) {
-	alert("f+");
-	var cashToUse = cash - priceFighter;
-	if (cashToUse>0) {
-		roundObject.playersCash = cash;
+function buyFighter() {
+	var cashToUse = roundObject.playersCash - priceFighter;
+	if (cashToUse > 0) {
+		roundObject.playersCash -= priceFighter;
+		roundObject.fightersToBuy++;
 		$('#credits').text(roundObject.playersCash + " $");
-		switch (planet) {
-		case 'atlantis':
-			roundObject.atlantis.newFighter++;
-			break;
-		case 'caprica':
-			roundObject.caprica.newFighter++;
-			break;
-		case 'coruscant':
-			roundObject.coruscant.newFighter++;
-			break;
-		case 'endor':
-			roundObject.endor.newFighter++;
-			break;
-		case 'erde':
-			roundObject.erde.newFighter++;
-			break;
-		case 'gemini':
-			roundObject.gemini.newFighter++;
-			break;
-		case 'tatooine':
-			roundObject.tatooine.newFighter++;
-			break;
-		default:
-			break;
-		}
+		$('#fighter').text(roundObject.fightersToBuy);
 	}
-
 }
+function buyBattlestar() {
+	var cashToUse = roundObject.playersCash - priceBattlestar;
+	if (cashToUse > 0) {
+		roundObject.playersCash -= priceBattlestar;
+		roundObject.battlestarsToBuy++;
+		$('#credits').text(roundObject.playersCash + " $");
+		$('#battlestar').text(roundObject.battlestarsToBuy);
+	}
+}
+function notBuyFighter(){
+	if(roundObject.fightersToBuy>0){
+		roundObject.fightersToBuy--;
+		roundObject.playersCash+=priceFighter;
+		$('#credits').text(roundObject.playersCash + " $");
+		$('#fighter').text(roundObject.fightersToBuy);
+	}
+}
+function notBuyBattlestar(){
+	if(roundObject.battlestarsToBuy>0){
+		roundObject.battlestarsToBuy--;
+		roundObject.playersCash+=priceBattlestar;
+		$('#credits').text(roundObject.playersCash + " $");
+		$('#battlestar').text(roundObject.battlestarsToBuy);
+	}
+}
+function fighterPlus(planet) {
+
+	$('#credits').text(roundObject.playersCash + " $");
+	switch (planet) {
+	case 'atlantis':
+		roundObject.atlantis.newFighter++;
+		break;
+	case 'caprica':
+		roundObject.caprica.newFighter++;
+		break;
+	case 'coruscant':
+		roundObject.coruscant.newFighter++;
+		break;
+	case 'endor':
+		roundObject.endor.newFighter++;
+		break;
+	case 'erde':
+		roundObject.erde.newFighter++;
+		break;
+	case 'gemini':
+		roundObject.gemini.newFighter++;
+		break;
+	case 'tatooine':
+		roundObject.tatooine.newFighter++;
+		break;
+	default:
+		break;
+	}
+}
+
 function fighterMinus(planet) {
 	switch (planet) {
 	case 'atlantis':
@@ -100,7 +129,7 @@ function fighterMinus(planet) {
 }
 function battlestarPlus(planet) {
 	var cashToUse = cash - priceBattlestar;
-	if (cashToUse>0) {
+	if (cashToUse > 0) {
 		cash -= priceBattlestar;
 		roundObject.playersCash = cash;
 		$('#credits').text(roundObject.playersCash + " $");
