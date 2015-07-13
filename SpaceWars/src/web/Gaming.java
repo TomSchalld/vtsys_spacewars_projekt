17f6c253fe31e8ff41e79b1286c27b9a9bf83f87
iPod90 <register@thomas-schalldach.de>
2015-07-13 11:55:48 +0200

    lots of new cool stuff

--- SpaceWars/src/web/Gaming.java
+++ SpaceWars/src/web/Gaming.java
@@ -1,13 +1,20 @@
 package web;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.json.JSONObject;
 
 import logic.Report;
 
@@ -30,14 +37,9 @@ public class Gaming extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		Map<String, String[]> params = request.getParameterMap();
-		for(String s[]:params.values()){
-			for(String string:s){
-				System.out.println(string);
-			}
-		}
-		
-		
+		System.out.println(request.getRequestedSessionId());
+		JSONObject roundObject = requestParamsToJSON(request);
+		System.out.println(roundObject.toString());
 		
 		
 		
@@ -47,10 +49,40 @@ public class Gaming extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		//TODO check if sessionID is valid
 		doGet(request, response);
 	}
-	
-	
+	private JSONObject requestParamsToJSON(HttpServletRequest request){
+		Map<String, String[]> params = request.getParameterMap();
+		JSONObject roundObject = new JSONObject();
+		JSONObject planetObject;
+		String planets[]={
+				"atlantis","caprica","coruscant","endor","erde","gemini","tatooine"
+			};
+		
+		
+		for(String s:planets){
+			planetObject = new JSONObject();
+			//System.out.println( params.get(s+"[newFighter]")[0]);
+			planetObject.put("newFighter", params.get(s+"[newFighter]")[0]);
+			planetObject.put("newBattlestar",params.get(s+"[newBattlestar]")[0]);
+			planetObject.put("sum",params.get(s+"[sum]")[0]);
+			roundObject.put(s, planetObject);
+		}
+		roundObject.put("fightersInStock",Integer.parseInt(params.get("fightersInStock")[0].trim()));
+		roundObject.put("battlestarsInStock",Integer.parseInt(params.get("battlestarsInStock")[0].trim()));
+		roundObject.put("fightersToBuy",Integer.parseInt(params.get("fightersToBuy")[0].trim()));
+		roundObject.put("battlestarsToBuy",Integer.parseInt(params.get("battlestarsToBuy")[0].trim()));
+		roundObject.put("playersCash",Integer.parseInt(params.get("playersCash")[0].trim()));
+		
+		
+		
+		return roundObject;
+		
+	}
+	private void doRound(JSONObject roundObject){
+		
+	}
 	
 	private void generateHighscore(Report endreport){
 		
