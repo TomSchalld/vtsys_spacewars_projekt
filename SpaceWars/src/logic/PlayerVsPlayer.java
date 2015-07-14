6e109e11db526dfa73420d65ab0c122e67f9bf01
iPod90 <register@thomas-schalldach.de>
2015-07-14 13:25:05 +0200

    fixed nullpointerexception

--- SpaceWars/src/logic/PlayerVsPlayer.java
+++ SpaceWars/src/logic/PlayerVsPlayer.java
@@ -14,87 +14,94 @@ public class PlayerVsPlayer extends UnicastRemoteObject implements Game {
 	protected final String gameName;
 	protected String passwort;
 	protected int gameId;
-	protected boolean hasEnoughPlayer=false;
+	protected boolean hasEnoughPlayer = false;
 	protected boolean gameFinished = false;
 	protected Client winner;
 	protected Client[] players;
 	protected Universe universe;
 	protected int round;
-	private static int gameCount=0;
+	private static int gameCount = 0;
 	protected Report endReport;
+
 	public PlayerVsPlayer(String gameName, int universeSize) throws RemoteException {
-		this.gameName=gameName;
-		this.gameId=gameCount;
-		this.universe=new Universe(universeSize);
+		this.gameName = gameName;
+		this.gameId = gameCount;
+		this.universe = new Universe(universeSize);
 		this.round = 0;
 		this.endReport = new EndReport();
 		gameCount++;
 		this.players = new Client[2];
-		
+
 	}
 
 	@Override
-	public void addPlayer(Client newPlayer) throws RemoteException{
-		if(this.players[0]==null){
-			this.players[0]=newPlayer;
-			System.out.println(newPlayer.getUsername()+" has logged in");
-		}else{
-			this.players[1]=newPlayer;
-			this.hasEnoughPlayer=true;
-			System.out.println(newPlayer.getUsername()+" has logged in");
+	public void addPlayer(Client newPlayer) throws RemoteException {
+		if (this.players[0] == null) {
+			this.players[0] = newPlayer;
+			System.out.println(newPlayer.getUsername() + " has logged in");
+		} else {
+			this.players[1] = newPlayer;
+			this.hasEnoughPlayer = true;
+			System.out.println(newPlayer.getUsername() + " has logged in");
 
 		}
 		newPlayer.setGamePlaying(this);
 
 	}
-	public void addPlayers(Client one, Client two) throws RemoteException{
+
+	public void addPlayers(Client one, Client two) throws RemoteException {
 		one.setGamePlaying(this);
 		two.setGamePlaying(this);
 		this.addPlayer(one);
 		this.addPlayer(two);
 	}
+
 	@Override
-	public boolean playersReady() throws RemoteException{
-		if(this.players[0].isPlayerReady()==true&&this.players[1].isPlayerReady()==true){
+	public boolean playersReady() throws RemoteException {
+		if (this.players[0].isPlayerReady() == true && this.players[1].isPlayerReady() == true) {
 			return true;
 		}
 		return false;
 	}
-	private void setPlayersUnready() throws RemoteException{
+
+	private void setPlayersUnready() throws RemoteException {
 		this.players[0].setPlayerReady(false);
-		this.players[1].setPlayerReady(false);				
+		this.players[1].setPlayerReady(false);
 	}
+
 	@Override
 	public RoundReport endRound() throws RemoteException {
-		if(this.playersReady()){
+		if (this.playersReady()) {
 			RoundReport report = new RoundReport();
-			Map<String,Planet>planets = this.getUniverse().getPlanets();
-			for(Planet p: planets.values()){
-				if(p.isFightAfterRoundEnded()){
+			Map<String, Planet> planets = this.getUniverse().getPlanets();
+			for (Planet p : planets.values()) {
+				if (p.isFightAfterRoundEnded()) {
 					report.addReport(p.fight());
 				}
 			}
-			for(Planet p: planets.values()){
-				if(!p.getShipsInOrbit().isEmpty()){
+			for (Planet p : planets.values()) {
+				if (!p.getShipsInOrbit().isEmpty()) {
 					p.getShipsInOrbit().get(0).getOwner().addCash(p.getGeneratedCredits());
 				}
 			}
 
 			this.setPlayersUnready();
-			for(Client c:this.players){
+			for (Client c : this.players) {
 				c.setRoundReport(report.exportRoundToJSON());
-				for(Planet p: this.getUniverse().getPlanets().values()){
-					if(p.getPlanetOwner().equals(c)){
-						c.sendAllShipsToStock(p);
+				for (Planet p : this.getUniverse().getPlanets().values()) {
+					if (p.getPlanetOwner() != null) {
+						if (p.getPlanetOwner().equals(c)) {
+							c.sendAllShipsToStock(p);
+						}
 					}
-					
+
 				}
 			}
 			this.round++;
-			if(this.players[0].getAmountOfPlanets()==this.getUniverse().getPlanets().size()){
+			if (this.players[0].getAmountOfPlanets() == this.getUniverse().getPlanets().size()) {
 				this.gameFinished = true;
 				this.winner = this.players[0];
-			}else if(this.players[1].getAmountOfPlanets()==this.getUniverse().getPlanets().size()){
+			} else if (this.players[1].getAmountOfPlanets() == this.getUniverse().getPlanets().size()) {
 				this.gameFinished = true;
 				this.winner = this.players[1];
 			}
@@ -133,9 +140,10 @@ public class PlayerVsPlayer extends UnicastRemoteObject implements Game {
 	public boolean killAllReferences() throws RemoteException {
 		return false;
 	}
+
 	@Override
 	public int getRound() throws RemoteException {
 		return round;
 	}
-	
+
 }
