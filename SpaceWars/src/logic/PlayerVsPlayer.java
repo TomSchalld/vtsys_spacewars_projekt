a1ed47dd0c73010e5bbf6d5ba2a6924285c15693
iPod90 <register@thomas-schalldach.de>
2015-07-21 23:46:31 +0200

    updt

--- SpaceWars/src/logic/PlayerVsPlayer.java
+++ SpaceWars/src/logic/PlayerVsPlayer.java
@@ -124,6 +124,8 @@ public class PlayerVsPlayer extends UnicastRemoteObject implements Game {
 				}
 			}
 			this.round++;
+			System.out.println(this.players[0].getUsername()+" anzahl planeten = "+this.players[0].getAmountOfPlanets());
+			System.out.println(this.players[1].getUsername()+" anzahl planeten = "+this.players[1].getAmountOfPlanets());
 			if (this.players[0].getAmountOfPlanets() == this.getUniverse().getPlanets().keySet().size()) {
 				this.gameFinished = true;
 				this.winner = this.players[0];
