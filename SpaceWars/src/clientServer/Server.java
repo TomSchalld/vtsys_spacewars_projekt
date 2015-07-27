038f65a8993549706ad8eec88619baac03d78ede
iPod90 <register@thomas-schalldach.de>
2015-07-27 19:23:25 +0200

    upsdr

--- SpaceWars/src/clientServer/Server.java
+++ SpaceWars/src/clientServer/Server.java
@@ -11,8 +11,22 @@ import logic.Human;
 
 
 public interface Server extends Remote {
+	/**<p>Puts the newGame into a Map of games, depends on how much players are already in the game</p>
+	 * @param newGame
+	 * @throws RemoteException
+	 * @throws Exception
+	 */
 	public void openGame(Game newGame) throws RemoteException, Exception;
+	/**<p>Brings the game to run if a new client has joined</p>
+	 * @param gameName
+	 * @throws RemoteException
+	 */
 	public void joinGame(String gameName)throws RemoteException;
+	/**<p>Returns a game by the gameName</p>
+	 * @param gameName
+	 * @return returns <p>Returns the game on success</p><strong>null</strong> if there is no game with that name
+	 * @throws RemoteException
+	 */
 	public Game getGameByName(String gameName)throws RemoteException;
 	public boolean closeGame(String username) throws RemoteException;
 	public Map<String, Game> gamesInLobby() throws RemoteException;
