597d42080003e594afe9acfef6021b50d596a4e9
iPod90 <register@thomas-schalldach.de>
2015-07-27 17:54:44 +0200

    updt

--- SpaceWars/src/helper/NameHelper.java
+++ SpaceWars/src/helper/NameHelper.java
@@ -8,8 +8,14 @@ package helper;
  *
  */
 public class NameHelper {
-	static final String serverAddress = "rmi://192.168.178.32:1099/GameServer";
+	static final String serverAddress = "rmi://192.168.178.23:1099/GameServer";
 
+	/**<p>
+	 * A method that returns the address of the bound GameServer
+	 * </p>
+	 * 
+	 * @return a String the address
+	 */
 	public static String getServeraddress() {
 		return serverAddress;
 	}
